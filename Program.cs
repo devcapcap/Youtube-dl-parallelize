fc6eb6de3ec6ba20c1eef603151fbb05ed986c23
unknown <b6bordel@yahoo.fr>
2022-02-25 00:26:10 +0100

    refactoring

--- Program.cs
+++ Program.cs
@@ -11,32 +11,63 @@ namespace Youtubedlparallelize
 {
     sealed class Program
     {
-        const string Exec = "youtube-dl.exe";
+        const string KeyExec = "ExecutableName";
+        const string msg = "can't not be found.";
+        private static string _executableName = null;
+        public static string ExecutableName
+        {
+            get
+            {
+                if (_executableName == null)
+                {
+                    var appConfs = System.Configuration.ConfigurationManager.AppSettings;
+                    if (appConfs.AllKeys.Contains(KeyExec))
+                        _executableName = appConfs[KeyExec];
+                }
+                return _executableName;
+            }
+        }
         static void Usage()
         {
-            Console.WriteLine($"{ Assembly.GetAssembly(typeof(Program)).GetName().Name} <filesList.txt>");
+            Console.WriteLine($"{ Assembly.GetAssembly(typeof(Program)).GetName().Name} <filesList.txt> optional : <destination path>");
         }
         static void Main(string[] args)
         {
             try
             {
-                if(!File.Exists(Exec))
+                if (!ParseArgument(ExecutableName, File.Exists))
                 {
-                    Console.WriteLine($"{Exec} can't not be found");
+                    Console.WriteLine($"{ExecutableName ?? "executable file"} {msg}");
                     Usage();
                     return;
                 }
                 string urlsList = null;
-                if ( args.Length == 0 
-                    || (string.IsNullOrWhiteSpace(args[0])
-                    && !File.Exists(args[0])))
+                string diretoryForDownload = null;
+                if (args.Length == 0)
                 {
-                    Console.WriteLine($"{urlsList} can't not be found");
                     Usage();
                     return;
                 }
-                urlsList = args[0];
-                RunAsync(urlsList).Wait();
+                if (args.Length > 0)
+                {
+                    bool hasError = false;
+                    urlsList = args[0];
+                    if ((hasError = !ParseArgument(urlsList, File.Exists)))
+                        Console.WriteLine($"{urlsList} {msg}");
+
+                    if (args.Length > 1)
+                    {
+                        diretoryForDownload = args[1];
+                        if ((hasError |= !ParseArgument(diretoryForDownload, Directory.Exists)))
+                            Console.WriteLine($"{diretoryForDownload} {msg}");
+                    }
+                    if (hasError)
+                    {
+                        Usage();
+                        return;
+                    }
+                }
+                RunAsync(urlsList, diretoryForDownload).Wait();
             }
             catch (Exception ex)
             {
@@ -46,9 +77,16 @@ namespace Youtubedlparallelize
             Console.WriteLine("press a key to exit program..");
             Console.ReadLine();
         }
-
-        private static Task<Process> RunProcessAsync(string exeFile,string arguments=null,
-                                                    string workingDirecty = null,CancellationToken cancellationToken = default(CancellationToken))
+        private static bool ParseArgument(string value, Func<string, bool> predicate)
+        {
+            if (!string.IsNullOrWhiteSpace(value) && predicate != null)
+                return predicate(value);
+            return false;
+        }
+        private static Task<Process> RunProcessAsync(string exeFile,
+                                                    string arguments = null,
+                                                    string workingDirecty = null,
+                                                    CancellationToken cancellationToken = default)
         {
             TaskCompletionSource<Process> taskCompletionSource = new TaskCompletionSource<Process>();
             Process process = new Process()
@@ -77,7 +115,8 @@ namespace Youtubedlparallelize
 
             return taskCompletionSource.Task;
         }
-        private static async Task<int> RunAsync(string filePath)
+
+        private static async Task<int> RunAsync(string filePath, string directoryForDownload)
         {
             if (!File.Exists(filePath))
             {
@@ -91,7 +130,7 @@ namespace Youtubedlparallelize
             for (int i = 0; i < tasks.Length; ++i)
             {
                 int tempI = i;
-                tasks[tempI] = RunProcessAsync(Exec,lines.ElementAt(tempI),Environment.CurrentDirectory);
+                tasks[tempI] = RunProcessAsync(ExecutableName, lines.ElementAt(tempI), directoryForDownload);
             }
             try
             {
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Youtube-dl-parallelize
 Allows multiple downloads with youtube-dl from a list of urls (text file) representing streams
-Open this project with visual studio 2017 or your prefered IDE or command line
+Open this project with visual studio 2017/2022 or your prefered IDE or command line
 
 How to use Youtube-dl-parallelize
 ---------------------------------
 Put the youtube-dl executable in the Youtube-dl-parallelize directory and enjoy (see the link below)
 In order to start a download from a text file with urls (on by line) : 
-Usage &lt;executable Youtube-dl-parallelize&gt; &lt;file_path_with_urls&gt;
+Usage &lt;executable Youtube-dl-parallelize&gt; &lt;file_path_with_urls&gt;  optional : &lt;destination path&gt;
 
 
 How to get youtube-dl
